d04519ed372f934dc9d291b903140949f8d05ea7
nameruse2 <nameruse@protonmail.com>
2020-07-22 22:35:22 +0100

    v0.1

--- exercises.py
+++ exercises.py
@@ -1,8 +1,19 @@
 exercises = {
-    "chest":["pushup"],
+    "chest":["pushup", "floor press"],
     "back":["pullup"],
     "legs":["squat", "lunge"],
-    "shoulders":["lateral raise"],
-    "arms":[],
-    "core":[]
+    "shoulders":["lateral raise", "overhead press"],
+    "arms":["curls"],
+    "core":["situp", "leg raise", "ab wheel"]
     }
+
+import random
+
+data = []
+
+for i in exercises:
+    data.append(random.choice(exercises[i]))
+
+random.shuffle(data)
+for i in data:
+    print(i)
